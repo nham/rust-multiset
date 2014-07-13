0f1427f1e5b45191baaa2614ccee51bb1241ee30
nham <hamann.nick@gmail.com>
2014-07-13 16:29:30 -0400

    Add some tests in main

--- src/mod.rs
+++ src/mod.rs
@@ -1,6 +1,27 @@
+#![feature(default_type_params)]
+
 extern crate collections;
 
+pub use std::iter::{FromIterator, Peekable};
 pub use collections::treemap::{TreeMap, TreeSet, Entries, RevEntries};
+use multiset::{Multiset, MutableMultiset, TreeMultiset};
+
 pub mod multiset;
 
-fn main() {}
+fn main() {
+    let v1 = vec!(4i, 5, 6);
+    let v2 = vec!(4i, 5, 6, 7);
+    let v3 = vec!(12i, 13);
+    let v4 = vec!(12i, 13, 6);
+
+    let xs: TreeMultiset<&int> = FromIterator::from_iter(v1.iter());
+    let ys: TreeMultiset<&int> = FromIterator::from_iter(v2.iter());
+    let zs: TreeMultiset<&int> = FromIterator::from_iter(v3.iter());
+    let ws: TreeMultiset<&int> = FromIterator::from_iter(v4.iter());
+
+    println!("{}", xs.is_subset(&ys));
+    println!("{}", xs.is_disjoint(&ys));
+    println!("{}", xs.is_disjoint(&zs));
+    println!("{}", ys.is_disjoint(&ws));
+
+}
